e6e6d243b39c5f697fcb6d6ea4f72f7d28e1a1fd
lonegunman <lonegunmanb@hotmail.com>
2021-07-13 14:05:03 +0800

    add ucloud provider

---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "ucloud" {
+  region      = var.ucloud_region
+  public_key  = var.ucloud_public_key
+  private_key = var.ucloud_private_key
+}
--- variables.tf
+++ variables.tf
@@ -1,12 +1,21 @@
 variable "sg_name" {
-  type = string
+  type    = string
   default = "ssr_sg"
 }
 variable "ssr_port" {
-  type = string
+  type    = string
   default = "443"
 }
 variable "accept" {
-  type = bool
+  type    = bool
   default = true
+}
+variable "ucloud_region" {
+  type = string
+}
+variable "ucloud_public_key" {
+  type = string
+}
+variable "ucloud_private_key" {
+  type = string
 }
